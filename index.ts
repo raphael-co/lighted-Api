7fdef779f2146947453c4a09f1ae6ba2730afe91
raphael-co <76869389+raphael-co@users.noreply.github.com>
2021-04-27 17:19:08 +0200

    cors

--- index.ts
+++ index.ts
@@ -9,7 +9,18 @@ import Client from './src/models/Client';
 config(); //process.env
 const app = express();
 
-app.use(cors())
+app.use(cors(
+    {
+      origin: true,
+      credentials: true,
+    }))
+  
+  app.use((req, res, next)=>
+  {
+      res.setHeader('Access-Control-Allow-Origin', '*') // Accepte les requetes des toutes origines
+      res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE') // Autoriser les méthodes
+      next()
+  })
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
